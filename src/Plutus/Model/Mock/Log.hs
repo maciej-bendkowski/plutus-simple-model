4360a3e85bb97f3c5947c91dbd83993988b8de79
Maciej Bendkowski <maciej.bendkowski@gmail.com>
2023-08-04 15:38:04 +0200

    Add `Eq` and `Show` instances to `Log`

--- src/Plutus/Model/Mock/Log.hs
+++ src/Plutus/Model/Mock/Log.hs
@@ -24,17 +24,17 @@ import Prelude
 
 -- | Log of Slot-timestamped events
 newtype Log a = Log {unLog :: Seq (Slot, a)}
-  deriving (Functor)
+  deriving (Functor, Eq, Show)
 
 instance Semigroup (Log a) where
   (<>) (Log sa) (Log sb) = Log (merge sa sb)
-    where
-      merge Empty b = b
-      merge a Empty = a
-      merge (a :<| as) (b :<| bs) =
-        if fst a <= fst b
-          then a Seq.<| merge as (b Seq.<| bs)
-          else b Seq.<| merge (a Seq.<| as) bs
+   where
+    merge Empty b = b
+    merge a Empty = a
+    merge (a :<| as) (b :<| bs) =
+      if fst a <= fst b
+        then a Seq.<| merge as (b Seq.<| bs)
+        else b Seq.<| merge (a Seq.<| as) bs
 
 -- | Insert event to log
 appendLog :: Slot -> a -> Log a -> Log a
@@ -50,9 +50,9 @@ fromLog (Log s) = toList s
 
 fromGroupLog :: Log a -> [(Slot, [a])]
 fromGroupLog = fmap toGroup . L.groupBy ((==) `on` fst) . fromLog
-  where
-    toGroup ((a, b) : rest) = (a, b : fmap snd rest)
-    toGroup [] = error "toGroup: Empty list"
+ where
+  toGroup ((a, b) : rest) = (a, b : fmap snd rest)
+  toGroup [] = error "toGroup: Empty list"
 
 instance Monoid (Log a) where
   mempty = Log Seq.empty
@@ -81,18 +81,18 @@ data MockEvent
 -- | Skip all info messages
 silentLog :: Log MockEvent -> Log MockEvent
 silentLog (Log xs) = Log $ Seq.filter (not . isInfo . snd) xs
-  where
-    isInfo = \case
-      MockInfo _ -> True
-      _ -> False
+ where
+  isInfo = \case
+    MockInfo _ -> True
+    _ -> False
 
 -- | Skip successful TXs
 failLog :: Log MockEvent -> Log MockEvent
 failLog (Log xs) = Log $ Seq.filter (not . isTx . snd) xs
-  where
-    isTx = \case
-      MockTx _ _ -> True
-      _ -> False
+ where
+  isTx = \case
+    MockTx _ _ -> True
+    _ -> False
 
 -- | filter by slot. Can be useful to filter out unnecessary info.
 filterSlot :: (Slot -> Bool) -> Log a -> Log a
